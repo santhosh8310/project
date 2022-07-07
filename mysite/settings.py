7b539b5f12ecb7e069d7f91aff195d2835992904
santhosh8310 <santhosh99981@gmail.com>
2022-07-07 08:49:38 +0530

    database setup done

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polling9',
+        'USER': 'groot',
+        'PASSWORD': 'Santhosh@8310720702',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
